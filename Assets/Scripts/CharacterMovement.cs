2980db3233763ced7d4f01df7d1b85174201c28e
CrashJumper <132718533+CrashJumper@users.noreply.github.com>
2023-05-18 23:42:11 -0400

    Movement with mouse click
    Created CharacterMovement script and connected for basic movement on mouse click.
    Bugs do exist:
    List out of index issue occasional when clicking new tile
    Character randomly starts animation over at starting tile

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField] Tile currentTile;
+    [SerializeField] List<Tile> movementList = new List<Tile>();
+
+    Vector3 fromPos = new Vector3 (0, 0, 0);
+    Vector3 toPos = new Vector3 (0, 0, 0);
+    float moveTime = 0f;
+    float movementTime = 1f;
+    void Start()
+    {
+        transform.position = new Vector3(currentTile.transform.position.x, currentTile.transform.position.y, transform.position.z);
+    }
+
+    void Update()
+    {
+        if(movementList.Count > 0)
+        {
+            MoveCharacter();
+        }    
+    }
+
+    public Tile CurrentTile()
+    {
+        return currentTile;
+    }
+
+    public void UpdateTile(Tile tile)
+    {
+        currentTile = tile;
+    }
+
+    public void SetMovementList(List<Tile> tiles)
+    {
+        movementList = tiles;
+        fromPos = transform.position;
+        toPos = new Vector3(movementList[0].transform.position.x, movementList[0].transform.position.y, transform.position.z);
+
+    }
+
+    void MoveCharacter()
+    {
+        if(transform.position == toPos)
+        {
+            currentTile = movementList[0];
+            movementList.RemoveAt(0);
+            if (movementList.Count == 0) return;
+            fromPos = transform.position;
+            toPos = new Vector3(movementList[0].transform.position.x, movementList[0].transform.position.y, transform.position.z);
+            moveTime = 0f;
+        }
+        transform.position = Vector3.Lerp(fromPos, toPos, (moveTime / movementTime));
+        moveTime += Time.deltaTime;
+    }
+}
--- Assets/Scripts/MouseSelection.cs
+++ Assets/Scripts/MouseSelection.cs
@@ -1,34 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class MouseSelection : MonoBehaviour
 {
-    //Temp
-    [SerializeField] Tile a;
-    [SerializeField] Tile b;
+    [SerializeField] Camera cam;
 
     [SerializeField] GameObject currentSelected = null;
+    [SerializeField] TileMap map;
+    [SerializeField] List<Tile> tiles = new List<Tile>();
+    void Start()
+    {
+        GameObject.FindWithTag("TileMap");
+    }
+
     // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            /*Ray ray = gameObject.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit))
+            RaycastHit2D[] hit  = Physics2D.RaycastAll(new Vector2(cam.ScreenToWorldPoint(Input.mousePosition).x, cam.ScreenToWorldPoint(Input.mousePosition).y), -Vector2.up, 0f);
+            if(hit.Length > 0)
             {
-                // the object identified by hit.transform was clicked
-                // do whatever you want
-            }*/
-
-            GameObject tileMap = GameObject.FindWithTag("TileMap");
-            List<Tile> tiles = tileMap.GetComponent<PathFinding>().FindPath(a, b);
-            for(int x = 0; x < tiles.Count; x++)
-            {
-                Debug.Log(tiles[x].name);
+                Debug.Log("Hit does not = null\n" + hit.Length.ToString());
+                if (hit[0].transform.CompareTag("Player"))
+                {
+                    Debug.Log("Player clicked");
+                    currentSelected = hit[0].transform.gameObject;
+                } 
+                if(hit[0].transform.CompareTag("Tile"))
+                {
+                    Debug.Log("Clicked Tile");
+                    if(currentSelected != null)
+                    {
+                        tiles.Clear();
+                        tiles = map.GetComponent<PathFinding>().FindPath(currentSelected.GetComponent<CharacterMovement>().CurrentTile(), hit[0].transform.gameObject.GetComponent<Tile>());
+                        currentSelected.GetComponent<CharacterMovement>().SetMovementList(tiles);
+                    }
+                }
             }
+        }
 
+        if (Input.GetMouseButtonDown(1))
+        {
+            currentSelected = null;
         }
     }
 }
